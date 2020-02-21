8649833c5808ade956369a2c446c4a7840e14012
Elophe <olivier.elophe@gmail.com>
2020-02-21 19:43:03 +0100

    settings

---
+++ version_amelioree/config/settings.py
@@ -0,0 +1,30 @@
+""" Ensemble des constantes utilisée dans l'application"""
+
+MAP_PATH = "data/labyrinthes/map.txt"
+FPS = 30
+ENEMY_MOVE_TIME = 0.5 # fréquence de mouvement ennemis
+
+SIZE_OF_SPRITE = 32
+
+NUMBER_OF_TILES = 4 # objets à collecter
+NUMBER_OF_ENEMIES = 2
+
+# Charactères utilisés pour encoder la map
+START_CHAR = 'S'
+END_CHAR = 'E'
+PATH_CHAR = '.'
+WALL_CHAR = '#'
+
+# Images
+HERO_IMAGE = "medias/perso.png"
+FOND_IMAGE = "medias/fond2.png"
+WALL_IMAGE = "medias/mur.png"
+GUARDIAN_IMAGE = "medias/Gardien.png"
+TILES_IMAGE = "medias/items2.png"
+ENEMIES_IMAGE = "medias/enemies.png"
+
+# Sons
+MUSIC = "medias/sound_track.mp3"
+SOUND_ITEM = "medias/item.wav"
+SOUND_WIN = "medias/win.wav"
+SOUND_LOOSE = "medias/loose.wav"
